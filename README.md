a699ee4d5c298b3eb231e4aa2a1617700adf633c
vlastanov <vlastanov@hotmail.com>
2020-05-15 10:42:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-menznd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-menznd)
---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[appHighlight]"
+})
+export class HighlightDirective {
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.backgroundColor = "yellow";
+  }
+
+  @HostListener("mouseenter")
+  onMouseEnter() {
+    this.highlight("yellow");
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
