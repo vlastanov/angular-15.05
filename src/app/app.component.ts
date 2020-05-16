69e351b353b8c2c40a25d2daa8551cf2263cdd58
vlastanov <vlastanov@hotmail.com>
2020-05-16 19:30:55 +0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,85 @@
-import { Component, VERSION,ViewChildren, QueryList,ElementRef,  ViewChild } from '@angular/core';
-import { NgModel } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import {
+  Component,
+  VERSION,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  ViewChild
+} from "@angular/core";
+import {
+  NgModel,
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+  Validators,
+  ValidatorFn
+} from "@angular/forms";
+import { Subscription } from "rxjs";
+
+import { debounceTime } from "rxjs/operators";
+
+function ratingRange(c: AbstractControl) {
+  let val = c.value < 0 || c.value > 5 || isNaN(c.value);
+  if (c.value !== null && val) {
+    return { outOfRange: true };
+  }
+  return null;
+}
+
+function myValidator(min: number, max: number): ValidatorFn {
+  let rangeRes = function ratingRange(c: AbstractControl) {
+    let val = c.value < min || c.value > max || isNaN(c.value);
+    if (c.value !== null && val) {
+      return { outOfRange: true };
+    }
+    return null;
+  };
+
+  return rangeRes;
+}
+
+function compareValidator(c: AbstractControl) {
+  let firstName = c.get("firstName");
+  let city = c.get("address.city");
+
+  if (firstName.pristine || city.pristine) {
+    return null;
+  }
+  if (firstName.value === city.value) {
+    return null;
+  }
+  return { match: true };
+}
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular1 ' + VERSION.major;
-  color = 'yellow'
+export class AppComponent {
+  customerForm: FormGroup;
+  firstNameMessage: string;
+
+  private validationMessages = {
+    required: "Please enter your first name",
+    minlength: "must be longer than 3 characters"
+  };
+
+  name = "Angular1 " + VERSION.major;
+  color = "yellow";
 
   filter1: string;
-  filter2: string; 
-  @ViewChildren('divElem,nameElement') inputElementRefs: QueryList<ElementRef>
-  @ViewChildren(NgModel) inputElementRefs2: QueryList<NgModel>
+  filter2: string;
+  @ViewChildren("divElem,nameElement") inputElementRefs: QueryList<ElementRef>;
+  @ViewChildren(NgModel) inputElementRefs2: QueryList<NgModel>;
 
+  constructor(private fb: FormBuilder) {}
 
-private _sub: Subscription
-  private _filterInput: NgModel
+  private _sub: Subscription;
+  private _filterInput: NgModel;
 
   get filterInput(): NgModel {
-    return this._filterInput
+    return this._filterInput;
   }
 
   @ViewChild(NgModel)
@@ -29,17 +87,56 @@ private _sub: Subscription
     // console.log(value)
     this._filterInput = value;
     if (this.filterInput && !this._sub) {
-      this._sub = this.filterInput.valueChanges.subscribe((changes) => {
-        this.filter1=changes;
-        return console.log(this.filter1)
-      }
-      )
+      this._sub = this.filterInput.valueChanges.subscribe(changes => {
+        this.filter1 = changes;
+        return console.log(this.filter1);
+      });
     }
   }
 
   onFilter2(event: string) {
-    this.filter2 = event
+    this.filter2 = event;
   }
 
+  ngOnInit(): void {
+    this.customerForm = this.fb.group(
+      {
+        firstName: ["", [Validators.required, Validators.minLength(3)]],
+        lastName: [{ value: "", disabled: true }],
+        ratingRange: [null, [myValidator(0, 8)]],
+        address: this.fb.group({
+          city: ["", [Validators.required, Validators.minLength(3)]]
+        })
+      },
+      { validator: compareValidator }
+    );
 
+    let firstname = this.customerForm.get("firstName");
+    firstname.valueChanges.pipe(debounceTime(1000)).subscribe(value => {
+      this.setFirstName(firstname);
+    });
+  }
+
+  setFirstName(c: AbstractControl) {
+    console.log(c.errors);
+    this.firstNameMessage = "";
+    if ((c.touched || c.dirty) && c.errors) {
+      this.firstNameMessage = Object.keys(c.errors)
+        .map(key => this.validationMessages[key])
+        .join(" ");
+    }
+  }
+
+  populateTestData() {
+    let firstname = this.customerForm.get("firstName");
+    // firstname.disable();
+    // firstname.setValidators(Validators.required);
+    // firstname.clearValidators()
+    // firstname.updateValueAndValidity
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log("Saved: " + JSON.stringify(this.customerForm.value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule ],
   declarations: [ AppComponent, HighlightDirective ],
   bootstrap:    [ AppComponent ]
 })
