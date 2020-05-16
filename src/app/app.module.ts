9fbf0768fce6bd4e3b023013665d81bf4c03252e
vlastanov <vlastanov@hotmail.com>
2020-05-16 20:04:02 +0300

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
+import { Test5Component } from './test5/test5.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule ],
-  declarations: [ AppComponent, HighlightDirective ],
+  declarations: [ AppComponent, HighlightDirective, Test5Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
