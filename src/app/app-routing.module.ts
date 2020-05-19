1024db7cd1a2e3c2bab9168b309a97e931bc6ede
vlastanov <vlastanov@hotmail.com>
2020-05-19 13:05:25 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { RouterModule, Routes } from '@angular/router';
-import { Play1Component } from './play1/play1.component';
-import { Play2Component } from './play2/play2.component';
+import { RouterModule, Routes } from "@angular/router";
+import { Play1Component } from "./play1/play1.component";
+import { Play2Component } from "./play2/play2.component";
 
 const routes: Routes = [
-   { path: 'play1', component: Play1Component },
-   { path: 'play2', component: Play2Component },
-  { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, // redirect to `first-component`
-   { path: '**', component: Play1Component },
-  //  { path: 'test5', component: Test5Component },
+  { path: "play2", component: Play2Component },
+  {
+    path: "play1",
+    component: Play1Component,
+    // children: [
+    //   {
+    //     path: "child-a",
+    //     component: ChildAComponent
+    //   },
+    //   {
+    //     path: "child-b",
+    //     component: ChildBComponent
+    //   }
+    // ]
+  },
+  { path: "", redirectTo: "/play1", pathMatch: "full" }, 
+  { path: "**", component: Play1Component }
   // { path: 'tablica', component: TablicaComponent, outlet: 'sidebar' },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-   exports: [RouterModule],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
