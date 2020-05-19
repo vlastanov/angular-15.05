d506f0b0cfa048d92ee038a7e50d0010f55e687e
vlastanov <vlastanov@hotmail.com>
2020-05-19 12:00:54 +0300

    2 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
+import { Play1Component } from './play1/play1.component';
 
 const routes: Routes = [
+   { path: 'play1', component: Play1Component },
+  //  { path: 'test5', component: Test5Component },
+  // { path: 'tablica', component: TablicaComponent, outlet: 'sidebar' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HighlightDirective } from "./highlight.directive";
 import { AppRoutingModule } from "./app-routing.module";
+import { Play1Component } from './play1/play1.component';
+
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HighlightDirective,
+    Play1Component,
   ],
   bootstrap: [AppComponent]
 })
